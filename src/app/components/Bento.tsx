5f4742210ee8a945cdbb5ddf96cc700193863211
Eduard <eduardsanguinocastro@gmail.com>
2024-01-26 09:40:48 +0100

    inicialized About component

--- src/app/components/Bento.tsx
+++ src/app/components/Bento.tsx
@@ -1,8 +1,10 @@
+import About from './About';
+
 const Bento = () => {
   return (
     <div className='grid__container  w-full h-[241.8rem] grid grid-cols-6  gap-[2rem] mt-[2rem]'>
       <div className='grid__me content shadow border-rad col-start-1 col-end-5 row-start-1 row-end-3'>
-        Presentación
+        <About />
       </div>
       <div className='grid__theme content shadow border-rad'>Dark Mode</div>
       <div className='grid__lang content shadow border-rad'>Idioma</div>
@@ -18,16 +20,16 @@ const Bento = () => {
       <div className='grid__projects col-start-1 col-end-7 row-start-4 row-end-5 content shadow border-rad'>
         Trabajos y Proyectos
       </div>
-      <div className='grid__skills col-start-1 col-end-7 content shadow border-rad'>
+      <div className='grid__skills col-start-1 col-end- content shadow border-rad'>
         Skills
       </div>
-      <div className='grid__animation col-start-1 col-end-7 content shadow border-rad'>
+      <div className='grid__animation col-start-1 col-end- content shadow border-rad'>
         Animación
       </div>
       <div className='grid__contact col-start-1 col-end-3 content shadow border-rad'>
         Contacto
       </div>
-      <div className='grid__form col-start-3 col-end-7 row-start-7 row-end-9 content shadow border-rad'>
+      <div className='grid__form col-start-3 col-end- row-start-7 row-end-9 content shadow border-rad'>
         Formulario
       </div>
       <div className='grid__cv col-start-1 col-end-3 content shadow border-rad'>
