351c31e98f95c8e5ba26013a43e5258d851df7af
Eduard <eduardsanguinocastro@gmail.com>
2024-04-08 12:04:55 +0200

    small updates

--- src/components/Animation.tsx
+++ src/components/Animation.tsx
@@ -1,6 +1,4 @@
 'use client';
-import Image from 'next/image';
-import image2 from '../assets/images/imagen2.png';
 
 import React, { useEffect } from 'react';
 
@@ -9,6 +7,7 @@ const Animation = () => {
     const el = document.getElementById('animation');
 
     const handleMouseMove = (e: MouseEvent) => {
+      if (typeof window === 'undefined') return;
       if (!el) return;
       const { clientHeight, clientWidth } = el;
 
