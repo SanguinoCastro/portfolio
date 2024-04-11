7afd17a14ab5f02650c12d7b087bc8d18b7d9da6
Eduard <eduardsanguinocastro@gmail.com>
2024-04-11 08:58:06 +0200

    repair bugs

--- src/app/context/DarkModeContext.tsx
+++ src/app/context/DarkModeContext.tsx
@@ -4,7 +4,7 @@ import React, { createContext, useState, useContext, useEffect } from 'react';
 
 interface DarkModeContextType {
   isDarkMode: boolean;
-  toggleDarkMode: () => void;
+  toggleDarkMode: (newPreference: boolean) => void;
 }
 
 const DarkModeContext = createContext<DarkModeContextType | undefined>(
@@ -33,28 +33,30 @@ if (typeof window !== 'undefined') {
 }
 
 // Establecemos el modo inicial en el que se cargará la web, tomando de referencia primero si hay configuración guardada en el local storage y sino toma la preferencia del navegador/ sistema Operativo:
+
+console.log('tipo de window:' + typeof window);
+
 export const DarkModeProvider: React.FC<DarkModeProviderProps> = ({
   children,
 }) => {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(() => {
-    const storedThemePreference = localStorage.getItem('themePreference');
-    const isDarkModePreferred =
-      typeof window !== 'undefined' &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const storedPreference = localStorage.getItem('themePreference');
     return (
-      storedThemePreference === 'true' ||
-      (!storedThemePreference && isDarkModePreferred)
+      storedPreference === 'true' || (!storedPreference && isDarkModePreferred)
     );
   });
 
-  const toggleDarkMode = () => {
-    setIsDarkMode((prevMode) => !prevMode);
-  };
-
   useEffect(() => {
-    if (window === undefined) return;
     localStorage.setItem('themePreference', isDarkMode.toString());
   }, [isDarkMode]);
+
+  const toggleDarkMode = (newPreference: boolean) => {
+    setIsDarkMode((prevIsDarkMode) => {
+      const updatedPreference =
+        newPreference !== undefined ? newPreference : !prevIsDarkMode;
+      return updatedPreference;
+    });
+  };
 
   const value: DarkModeContextType = {
     isDarkMode,
@@ -67,3 +69,73 @@ export const DarkModeProvider: React.FC<DarkModeProviderProps> = ({
     </DarkModeContext.Provider>
   );
 };
+// import React, { createContext, useState, useContext, useEffect } from 'react';
+
+// interface DarkModeContextType {
+//   isDarkMode: boolean;
+//   toggleDarkMode: () => void;
+// }
+
+// const DarkModeContext = createContext<DarkModeContextType | undefined>(
+//   undefined
+// );
+
+// export const useDarkMode = () => {
+//   const context = useContext(DarkModeContext);
+//   if (!context) {
+//     throw new Error(
+//       'useDarkMode debe ser utilizado dentro de un DarkModeProvider'
+//     );
+//   }
+//   return context;
+// };
+
+// interface DarkModeProviderProps {
+//   children: React.ReactNode;
+// }
+
+// var isDarkModePreferred = false;
+// if (typeof window !== 'undefined') {
+//   isDarkModePreferred = window.matchMedia(
+//     '(prefers-color-scheme: dark)'
+//   ).matches;
+// }
+
+// // Establecemos el modo inicial en el que se cargará la web, tomando de referencia primero si hay configuración guardada en el local storage y sino toma la preferencia del navegador/ sistema Operativo:
+
+// console.log('tipo de window:' + typeof window);
+
+// export const DarkModeProvider: React.FC<DarkModeProviderProps> = ({
+//   children,
+// }) => {
+//   const [isDarkMode, setIsDarkMode] = useState<boolean>(() => {
+//     const storedThemePreference = localStorage.getItem('themePreference');
+//     const isDarkModePreferred =
+//       typeof window !== 'undefined' &&
+//       window.matchMedia('(prefers-color-scheme: dark)').matches;
+//     return (
+//       storedThemePreference === 'true' ||
+//       (!storedThemePreference && isDarkModePreferred)
+//     );
+//   });
+
+//   const toggleDarkMode = () => {
+//     setIsDarkMode((prevMode) => !prevMode);
+//   };
+
+//   useEffect(() => {
+//     if (window === undefined) return;
+//     localStorage.setItem('themePreference', isDarkMode.toString());
+//   }, [isDarkMode]);
+
+//   const value: DarkModeContextType = {
+//     isDarkMode,
+//     toggleDarkMode,
+//   };
+
+//   return (
+//     <DarkModeContext.Provider value={value}>
+//       {children}
+//     </DarkModeContext.Provider>
+//   );
+// };
