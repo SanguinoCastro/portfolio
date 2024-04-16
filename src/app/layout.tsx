00c65a9ebb337431dba05986686265c7f1d552e3
Eduard <eduardsanguinocastro@gmail.com>
2024-04-16 11:05:56 +0200

    add analytics vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 // import { Inter } from 'next/font/google';
 import './styles/normalize.css';
 import './styles/globals.css';
@@ -63,6 +64,7 @@ export default function RootLayout({
             </main>
           </LanguageProvider>
         </DarkModeProvider>
+        <Analytics />
       </body>
     </html>
   );
